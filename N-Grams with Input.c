b01034ed2df425dc387143de27f8f793284a2164
adityatresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2020-04-17 19:11:28 +0700

    Update N-Grams with Input.c

--- N-Grams with Input.c
+++ N-Grams with Input.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : 6
+* Hari dan Tanggal : Kamis, 16 April 2020
+* Asisten (NIM) : Lionel Valdarant (18316020)
+* Nama File : input.c
+* Deskripsi : mengubah kata-kata pada file eksternal menjadi tabel n-gram beserta kode untuk menerima input user
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
