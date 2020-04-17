b3ea129bbd3dfe5c77c8ae41be578bdac8651149
adityatresnobudi <60229333+adityatresnobudi@users.noreply.github.com>
2020-04-17 19:10:41 +0700

    Update input.c

--- input.c
+++ input.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : 6
+* Hari dan Tanggal : Kamis, 16 April 2020
+* Asisten (NIM) : Lionel Valdarant (18316020)
+* Nama File : input.c
+* Deskripsi : berisi kode untuk menerima input user
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,9 +23,4 @@ void input_user()
     }
     printf("Berapa banyak kata random yang ingin dibentuk : ");
     scanf("%d", RandWord);
-    /* while (RandWord < 2000) //minimal mencetak dari 1 key saja
-    {
-        printf("Banyak kata random minimal 2000! Silahkan ulangi");
-        scanf("%d", RandWord);
-    }*/
 }
