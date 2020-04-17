e73436ed3b888cee79b5b54918f1fee7afc4e549
puspaduhita <49947654+puspaduhita@users.noreply.github.com>
2020-04-17 18:51:42 +0700

    Add files via upload

--- tubes pemecahan masalah.c
+++ tubes pemecahan masalah.c
@@ -2,248 +2,194 @@
 #include <stdlib.h>
 #include <string.h>
 #include "boolean.h"
+#include "queue_masalah.h"
 #define Nil NULL
 
+ngram* OpenFile();
 
-typedef struct
-{
-	char key[100];
-	char value[100];
-}
-gram;
-
-typedef struct node{
-	char valueq[100];
-	struct node* next;
-} node;
-
-typedef struct {
-	node* head;
-	node* tail;
-} Queue;
-
-typedef struct {
-	char huruf[100];
-} kata;
-
-void createEmpty(Queue* Q);
-node* Alokasi(char[]);
-void Push(Queue* Q, char[]);
-void Pop(Queue* Q, char[]);
-void Print(Queue*);
-
-//n itu berapa banyak tabel gram yang dibentuk
-void Search (char key_current[], gram tabel_gram[],int index_found_array[],int* count,int N){
-	
-	int i,j;
-	i =0;
-	j =0;
-	*count = 0;
-	printf("Masuk ke search\n");
-	
-	while(i<N){
-		if(strcmp(tabel_gram[i].key,key_current)==0){
-			index_found_array[j] = i;
-			printf("nemu nih\n");
-			j++;
-		}
-		i++;
-	}
-	*count = j-1;
-	
-	
-}
-
-void generate (char key_current[], int* banyak_kata, Queue* Q, gram tabel_gram[], int N){
-	int index_found_array[10];
-	int index_found;
-	int banyak_kemunculan;
-	int length;
-	//Basis
-	if (*banyak_kata==0){
-		// gak ush diprint win, tapi kek kasih kata-kata udh berhasil di generate
-		printf("n_gram berhasil di generate\n");
-	}
-	
-	//Rekurens
-	else
-	{
-		char* token[10];
-		int i;
-		Search(key_current,tabel_gram,index_found_array,&banyak_kemunculan,N); //
-		
-		//klo banyak_kemunculan>1
-		if(banyak_kemunculan>1) {
-			// randomize
-			// ini belum win
-			printf("random\n");
-			
-		}
-		else{
-			index_found = index_found_array[0];
-		}
-		
-		token[0] = strtok(key_current, " ");
-
-		i =0;
-		while (token[i] != NULL)
-		{
-			i =i+1;
-			token[i] = strtok(NULL, " ");
-			
-		}
-		length = i;
-		Push(Q,tabel_gram[index_found].value);
-		
-		//tolong dirapiin
-		strcpy(key_current, token[1]);
-		strcat(key_current, " ");
-		
-		for(i=2;i<length;i++){
-			strcat(key_current, token[i]);
-			strcat(key_current, " ");
-		}
-		
-		strcat(key_current, tabel_gram[index_found].value);
-		//strcat(key_current, " ");
-		(*banyak_kata)--;
-		generate (key_current,banyak_kata, Q,tabel_gram, N);
-	}
-}
-/* ini mau dicoba2 aja
-void handling_file(char file_name[100], n_gram tabel_gram[], int banyak_gram){
-	FILE *file;
-	kata daftar_kata[1000];
-	char* token[100];
-	char stream[100];
-	
-	file = fopen(filename,"r");
-	if(file!=Nil){
-		while(fscanf(file,stream)!=EOF){
-			
-			
-		}
-	}
-}
-*/
+// List variabel
+int PanjangList=0;
+char *filename;
+int n;
+int RandWord;
+ngram *NList;
 
 
 int main(){
+	int c, i, pil1;
 	Queue Q;
-	char X[100];
-	createEmpty(&Q);
-	int i,banyak_kata;
-	// ini dihapus nanti
-	int index_found_array[10];
-	int count;
-	
-	
-	gram tabel_gram[100];
-	/*
-	strcpy(tabel_gram[0].key,"Ships at ");
-	strcpy(tabel_gram[0].value,"a ");
-	strcpy(tabel_gram[1].key,"at a ");
-	strcpy(tabel_gram[1].value,"distance ");
-	strcpy(tabel_gram[2].key,"a distance ");
-	strcpy(tabel_gram[2].value,"have ");
-	strcpy(tabel_gram[3].key,"distance have ");
-	strcpy(tabel_gram[3].value,"every ");
-	strcpy(tabel_gram[4].key,"have every ");
-	strcpy(tabel_gram[4].value,"man ");
-	strcpy(tabel_gram[5].key,"every man ");
-	strcpy(tabel_gram[5].value,"wish ");
-	strcpy(tabel_gram[6].key,"man wish ");
-	strcpy(tabel_gram[6].value,"on ");
-	strcpy(tabel_gram[7].key,"wish on ");
-	strcpy(tabel_gram[7].value,"board. ");
-	strcpy(tabel_gram[8].key,"on board. ");
-	strcpy(tabel_gram[8].value,"Ships ");
-	strcpy(tabel_gram[9].key,"board. Ships ");
-	strcpy(tabel_gram[9].value,"at ");
-	*/
-	
-	
-	char string_awal[] = "distance have ";
-	char key_current[100];
-	strcpy(key_current,string_awal);
-	banyak_kata = 20-2;
-	generate (key_current, &banyak_kata, &Q,tabel_gram,10);
-	printf("%s",string_awal);
-	Print(&Q);
-	
+	char* X;
+	char pil;
+
+	printf("----------MAIN MENU----------\n");
+    printf("Program ini akan menerima sebuah input n dari pengguna yang\n");
+    printf("harus bernilai 2 atau lebih agar tidak menghasilkan\n");
+    printf("kalimat random. Kemudian, pengguna akan diminta memasukkan jumlah\n");
+    printf("kata random yang ingin dicetak(haruslah bernilai 2000 atau lebih).\n");
+    printf("Silakan mencoba~~\n");
+    printf("Tekan huruf Y untuk memulai program, dan tekan huruf X untuk keluar.\n");
+    scanf("%c", &pil);
+
+    if (pil=='Y'||pil=='y')
+    do
+    {
+        filename = malloc(sizeof(char));
+        filename[0]='\0';
+        printf("Masukkan nama file : ");
+        scanf("%s", filename);
+
+        printf("\nMasukkan nilai N untuk N gram : ");
+        scanf("%d", &n);
+        while (n < 2) //cek masukan nilai n
+        {
+            printf("Masukkan nilai N tidak boleh dibawah 2! Silahkan ulangi");
+            scanf("%d", n);
+        }
+
+        printf("Berapa banyak kata random yang ingin dibentuk : ");
+        scanf("%d", &RandWord); //RandWord == banyak kata
+
+
+        createEmpty(&Q);
+
+        // ini dihapus nanti
+        //int* index_found_array;
+        //index_found_array = (int*)malloc(100*sizeof(int));
+        OpenFile(); // Pembentukan array ngram di main
+
+        char* string_awal;
+        char* key_current;
+        string_awal = (char*) malloc (100*n*sizeof(char));
+        X = (char*) malloc (100*sizeof(char));
+        key_current = (char*) malloc (n*100*sizeof(char));
+        strcpy(string_awal,NList[2].key);
+        strcpy(key_current,string_awal);
+
+        generate (key_current, &RandWord, &Q, NList, PanjangList);
+
+        printf("...");
+        printf("%s",string_awal);
+        Print(&Q);
+        printf("...");
+
+        printf("\nApakah pengguna ingin memasukkan jumlah kata lagi?\n");
+        printf("Tekan 1 untuk memasukkan kata lagi, 0 untuk kembali ke main menu.\n");
+        scanf("%d", &pil1);
+        if (pil1==1)
+        do
+        {
+            createEmpty(&Q);
+
+            // ini dihapus nanti
+            //int* index_found_array;
+            //index_found_array = (int*)malloc(100*sizeof(int));
+            OpenFile(); // Pembentukan array ngram di main
+
+            char* string_awal;
+            char* key_current;
+            string_awal = (char*) malloc (100*n*sizeof(char));
+            X = (char*) malloc (100*sizeof(char));
+            key_current = (char*) malloc (n*100*sizeof(char));
+            strcpy(string_awal,NList[2].key);
+            strcpy(key_current,string_awal);
+
+            generate (key_current, &RandWord, &Q, NList, PanjangList);
+
+            printf("...");
+            printf("%s",string_awal);
+            Print(&Q);
+            printf("...");
+
+        } while (pil1==1);
+        //free(index_found_array);
+        free(X);
+        free(key_current);
+        free(string_awal);
+
+        free(NList);
+    } while (pil=='Y'||pil=='y');
+    return 0;
 }
 
+ngram* OpenFile(){
+    // Open File
+    FILE *fp = fopen(filename,"r");
+    if(!fp){ //Bila file tidak terbuka
+        printf("File tidak terbuka\n");
+        fclose(fp);
+        return NList;
+    }
 
-node* Alokasi(char X[]){
-	node* P;
-	P = (node*) malloc(sizeof(node));
-	if(P==Nil){
-		return Nil;
+    rewind(fp); // Meletekkan cursor di awal file
+    char word[1024]; // Variabel karakter yang menyimpan pembacaan baris pada csv
+
+    /* Percobaan 1 */
+    NList = malloc(sizeof(ngram)); // Pembentukan variabel ngram
+
+    char *words;
+    for(int i=0; i<n; i++){
+        fscanf(fp," %1023s", word);
+        if(i==0){
+            words = strdup(word);
+            strcat(words," ");
+        }
+        else{
+            strcat(words,word);
+            strcat(words," ");
+        }
+    }
+    int i=0;
+	char tmp[1024];
+    while(fscanf(fp," %1023s", word) == 1){ // Pembacaaan kata satu per satu pada file
+        NList = realloc(NList,(i+1)*sizeof(ngram)); // Alokasi memori variabel ngram
+        int j=0;
+
+        if(i==0){ // Pembentukan ngram pertama
+            memset(tmp,'\0',sizeof(tmp));
+            strcpy(tmp, words);
+            NList[i].key = strdup(tmp);
+            NList[i].value = strdup(word);
+            i++;
+        }
+        else{
+            strcpy(tmp, NList[i-1].key);
+            strcat(tmp, NList[i-1].value);
+
+            words = strchr(tmp, ' ');
+            if(words != NULL){
+                words = words + 1;
+            }
+            strcpy(tmp, words);
+            strcat(tmp, " ");
+            NList[i].key = strdup(tmp);
+            NList[i].value = strdup(word);
+            i++;
+        }
+    }
+
+	int j;
+	for(j=i;j<i+n;j++){
+		char* token;
+		char* temp_awal;
+
+		NList = realloc(NList,(j+1)*sizeof(ngram));
+		strcpy(tmp, NList[j-1].key);
+        strcat(tmp, NList[j-1].value);
+		words = strchr(tmp, ' ');
+        if(words != NULL){
+            words = words + 1;
+        }
+        strcpy(tmp, words);
+        strcat(tmp, " ");
+        NList[j].key = strdup(tmp);
+
+		temp_awal = strdup(NList[j-i].key);
+		token = strtok(temp_awal," ");
+		NList[j].value = strdup(token);
 	}
-	else{
-		strcpy(P->valueq,X);
-		P->next = Nil;
-		return P;
-	}
+
+    // Akhir kalimat pada file
+    PanjangList = j;
+    fclose(fp);
+    return NList;
 }
-
-void createEmpty(Queue* Q){
-	(*Q).head = Nil;
-	(*Q).tail = Nil;
-}
-
-void Push (Queue* Q, char X[]){
-/*sudah ditangani jika stack kosong */
-	node* P;
-	P = Alokasi(X);
-	if(P!=Nil){
-		if((*Q).head==Nil){ //jika kosong
-			(*Q).head = P;
-			(*Q).tail = P;
-		}
-		else{
-			((*Q).tail)->next = P;
-			(*Q).tail = P;
-		}
-	}
-}
-
-//HATI-HATI
-void Pop (Queue* Q, char X[]){
-/*sudah ditangani jika stack kosong*/
-	node* P;
-	if((*Q).head==Nil){
-			printf("Tidak dapat melakukan pop\n");
-	}
-	else{
-		strcpy(X ,((*Q).head)->valueq);
-		P=(*Q).head;
-		(*Q).head = P->next;
-		if((*Q).head==Nil) // jika ketika di pop jadi kosong
-			(*Q).tail = Nil;
-		P->next = Nil;
-		free(P);
-	}
-}
-
-boolean isEmpty(Queue Q){
-	if(Q.head==Nil){
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-void Print(Queue* Q){
-	char X[100];
-	while(!isEmpty(*Q)){
-		Pop(Q,X);
-		printf("%s",X);
-	}
-	
-}
-
-		
-		
-	
