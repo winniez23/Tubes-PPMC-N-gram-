8de32b71f7c7ab6a6431bab55e2c7d8fbde76ec9
puspaduhita <49947654+puspaduhita@users.noreply.github.com>
2020-04-17 20:32:20 +0700

    Add files via upload

---
+++ EL2208_Tubes_6/queue_masalah.c
@@ -0,0 +1,133 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : 6
+* Hari dan Tanggal : Kamis, 16 April 2020
+* Asisten (NIM) : Lionel Valdarant (18316020)
+* Nama File : input.c
+* Deskripsi : menimbun isi tabel yang terbentuk
+*/
+
+#include "queue_masalah.h"
+#include "boolean.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//n itu berapa banyak tabel gram yang dibentuk
+
+
+
+void generate (int* banyak_kata,int panjangList, Queue* Q, ngram* tabel_gram, int* start){ //menghasilkan queue yang berisi kata2 yang telah digenerate berdasarkan tabel n-gram
+
+	//Basis
+	int sth;
+	if (*banyak_kata==0){
+		if((*start)<panjangList){
+			Push(Q,tabel_gram[(*start)].value); //masukkan value tersebut ke queue
+			printf("%s ", tabel_gram[(*start)].value);
+		}
+		
+		else{
+			//printf("ini kawab i : %d",i);
+			//Push(Q,tabel_gram[(*start) - panjangList-1].value); //masukkan value tersebut ke queue
+			sth = (*start) - panjangList;
+			printf("%s ", tabel_gram[sth].value);
+		}
+		printf("...\n");
+	}
+	//Rekurens
+	else{
+		
+		if((*start)<panjangList){
+			Push(Q,tabel_gram[(*start)].value); //masukkan value tersebut ke queue
+			printf("%s ", tabel_gram[(*start)].value);
+		}
+		else if ((*start)>=panjangList){
+			
+			sth = (*start) - panjangList;
+			printf("%s ", tabel_gram[sth].value);
+			Push(Q,tabel_gram[sth].value); //masukkan value tersebut ke queue
+		}
+		(*banyak_kata)--;
+		(*start)++;
+		
+		//rekursif
+		
+		generate (banyak_kata,panjangList, Q, tabel_gram,start);
+	}
+}
+
+node* Alokasi(char* X){
+	node* P;
+	P = (node*) malloc(sizeof(node));
+	if(P==Nil){
+		return Nil;
+	}
+	else{
+		P->valueq = (char*) malloc(100*sizeof(char));
+		strcpy(P->valueq,X);
+		P->next = Nil;
+		return P;
+	}
+}
+
+void createEmpty(Queue* Q){
+	(*Q).head = Nil;
+	(*Q).tail = Nil;
+}
+
+void Push (Queue* Q, char* X){
+/*sudah ditangani jika stack kosong */
+	node* P;
+	P = Alokasi(X);
+	if(P!=Nil){
+		if((*Q).head==Nil){ //jika kosong
+			(*Q).head = P;
+			(*Q).tail = P;
+		}
+		else{
+			((*Q).tail)->next = P;
+			(*Q).tail = P;
+		}
+	}
+}
+
+//HATI-HATI
+void Pop (Queue* Q, char* X){
+/*sudah ditangani jika stack kosong*/
+	node* P;
+	if((*Q).head==Nil){
+			printf("Tidak dapat melakukan pop\n");
+	}
+	else{
+		strcpy(X ,((*Q).head)->valueq);
+		P=(*Q).head;
+		(*Q).head = P->next;
+		if((*Q).head==Nil) // jika ketika di pop jadi kosong
+			(*Q).tail = Nil;
+		P->next = Nil;
+		free(P);
+	}
+}
+
+boolean isEmpty(Queue Q){
+	if(Q.head==Nil){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void Print(Queue* Q){
+	char X[100];
+	while(!isEmpty(*Q)){
+		Pop(Q,X);
+		printf("%s ",X);
+	}
+	
+}
+
+		
+		
+	
---
+++ EL2208_Tubes_6/tubes pemecahan masalah.c
@@ -0,0 +1,164 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : 6
+* Hari dan Tanggal : Kamis, 16 April 2020
+* Asisten (NIM) : Lionel Valdarant (18316020)
+* Nama File : input.c
+* Deskripsi : Berisi main program dan pemecahan masalah
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "boolean.h"
+#include "queue_masalah.h"
+#define Nil NULL
+
+ngram* OpenFile();
+
+// List variabel
+long int PanjangList=0;
+char *filename;
+int n;
+int RandWord;
+ngram *NList;
+int main(){
+	int c,pil1;
+	Queue Q;
+	int i;
+	int banyak_kata;
+	int start;
+	char pil='Y';
+
+	while(pil=='Y' || pil=='y'){
+        printf("----------MAIN MENU----------\n");
+        printf("Program ini akan menerima sebuah input n dari pengguna yang\n");
+        printf("harus bernilai 2 atau lebih agar tidak menghasilkan\n");
+        printf("kalimat random. Kemudian, pengguna akan diminta memasukkan jumlah\n");
+        printf("kata random yang ingin dicetak(haruslah bernilai 2000 atau lebih).\n");
+        printf("Silakan mencoba~~\n");
+        printf("Tekan huruf Y untuk memulai program, dan tekan huruf X untuk keluar.\n");
+        scanf("%c", &pil);
+
+        filename = (char*)malloc(100*sizeof(char));
+        printf("Masukkan nama file : ");
+        scanf("%s", filename);
+
+        printf("Masukkan nilai N untuk N gram : ");
+        scanf("%d", &n);
+        while (n < 2) //cek masukan nilai n
+        {
+            printf("Masukkan nilai N tidak boleh dibawah 2! Silahkan ulangi");
+            scanf("%d", n);
+        }
+        printf("Berapa banyak kata random yang ingin dibentuk : ");
+        scanf("%d", &RandWord); //RandWord == banyak kata
+        pil1 = 1;
+        while(pil1 == 1){
+            OpenFile();
+            if(NList!=NULL){
+                createEmpty(&Q);
+                //start = rand() %(PanjangList + 1 - 0) + 0;
+                start = 3;
+                printf("... %s",NList[start].key);
+                banyak_kata = RandWord-n;
+                generate (&banyak_kata,PanjangList,&Q,NList, &start);
+                //Print(&Q);
+                printf("\nApakah pengguna ingin memasukkan jumlah kata lagi?\n");
+                printf("Tekan 1 untuk memasukkan kata lagi, 0 untuk kembali ke main menu.\n");
+                scanf("%d", &pil1);
+            }
+        }
+        free(NList);
+        printf("Tekan huruf Y untuk memulai program, dan tekan huruf X untuk keluar.\n");
+        scanf("%trash", &pil);
+        scanf("%c", &pil);
+        system("cls");
+	}
+
+	return 0;
+}
+
+ngram* OpenFile(){
+    // Open File
+    FILE *fp = fopen(filename,"r");
+    if(!fp){ //Bila file tidak terbuka
+        printf("File tidak terbuka\n");
+        fclose(fp);
+        return NList;
+    }
+
+    rewind(fp); // Meletekkan cursor di awal file
+    char word[1024]; // Variabel karakter yang menyimpan pembacaan baris pada csv
+
+    /* Percobaan 1 */
+    NList = malloc(sizeof(ngram)); // Pembentukan variabel ngram
+
+    char *words;
+    for(int i=0; i<n; i++){
+        fscanf(fp," %1023s", word);
+        if(i==0){
+            words = strdup(word);
+            strcat(words," ");
+        }
+        else{
+            strcat(words,word);
+            strcat(words," ");
+        }
+    }
+    int i=0;
+	char tmp[1024];
+    while(fscanf(fp," %1023s", word) == 1){ // Pembacaaan kata satu per satu pada file
+        NList = realloc(NList,(i+1)*sizeof(ngram)); // Alokasi memori variabel ngram
+        int j=0;
+
+        if(i==0){ // Pembentukan ngram pertama
+            memset(tmp,'\0',sizeof(tmp));
+            strcpy(tmp, words);
+            NList[i].key = strdup(tmp);
+            NList[i].value = strdup(word);
+            i++;
+        }
+        else{
+            strcpy(tmp, NList[i-1].key);
+            strcat(tmp, NList[i-1].value);
+
+            words = strchr(tmp, ' ');
+            if(words != NULL){
+                words = words + 1;
+            }
+            strcpy(tmp, words);
+            strcat(tmp, " ");
+            NList[i].key = strdup(tmp);
+            NList[i].value = strdup(word);
+            i++;
+        }
+    }
+
+	int j;
+	for(j=i;j<i+n;j++){
+		char* token;
+		char* temp_awal;
+
+		NList = realloc(NList,(j+1)*sizeof(ngram));
+		strcpy(tmp, NList[j-1].key);
+        strcat(tmp, NList[j-1].value);
+		words = strchr(tmp, ' ');
+        if(words != NULL){
+            words = words + 1;
+        }
+        strcpy(tmp, words);
+        strcat(tmp, " ");
+        NList[j].key = strdup(tmp);
+
+		temp_awal = strdup(NList[j-i].key);
+		token = strtok(temp_awal," ");
+		NList[j].value = strdup(token);
+	}
+
+    // Akhir kalimat pada file
+    PanjangList = j;
+    fclose(fp);
+    return NList;
+}
